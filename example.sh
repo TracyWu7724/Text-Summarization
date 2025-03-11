1e5b9e7d8f062234393875948b0716611ec808c4
Tracy Wu <wutracy3@163.com>
2025-03-11 13:24:56 -0400

    Create an example.sh to test API

---
+++ example.sh
@@ -0,0 +1,4 @@
+
+curl -X POST "https://2344b39xhe.execute-api.us-east-2.amazonaws.com/dev/summarize/" \
+     -H "Content-Type: application/json" \
+     -d '{"text": "Altria Group, Inc., through its subsidiaries, manufactures and sells smokeable and oral tobacco products in the United States. The company offers cigarettes primarily under the Marlboro brand; large cigars and pipe tobacco under the Black & Mild brand; moist smokeless tobacco and snus products under the Copenhagen, Skoal, Red Seal, and Husky brands; oral nicotine pouches under the on! brand; and e-vapor products under the NJOY ACE brand. It sells its products to distributors, as well as large retail organizations, such as chain stores. The company was founded in 1822 and is headquartered in Richmond, Virginia."}'
